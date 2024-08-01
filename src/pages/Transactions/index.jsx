c4db36d499913df959d6fa38e28ddc4a4c9abf7f
augustoaccorsi <augusto.acorsi@gmail.com>
2024-07-31 21:18:06 -0300

    add transacton table

--- src/pages/Transactions/index.jsx
+++ src/pages/Transactions/index.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Summary from '../../components/Summary';
+import {
+    TransactionsContainer,
+    TransactionsTable,
+    PriceHighlight,
+} from './styles';
 
 const Transactions = () => {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <td width="50%">Website Development</td>
+                        <td>
+                            <PriceHighlight variant="income">
+                                R$ 12,000.00
+                            </PriceHighlight>
+                        </td>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                    </tbody>
+                    <tbody>
+                        <td width="50%">Hamburguer</td>
+                        <td>
+                            <PriceHighlight variant="outcome">
+                                -R$ 50.00
+                            </PriceHighlight>
+                        </td>
+                        <td>Alimentaçao</td>
+                        <td>13/04/2022</td>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     );
 };
